2be9c6bae9e305973e150f4a1499deb2ac684b48
Alex Klimashevsky <gladiator1988@gmail.com>
2017-03-17 23:52:47 +0300

    ok poles

--- src/poles/Solution.java
+++ src/poles/Solution.java
@@ -7,8 +7,6 @@ import java.util.*;
  */
 class Solution {
 
-    private long[] saved;
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -25,42 +23,30 @@ class Solution {
     }
 
     long cost(int k, List<Pole> poles) {
-        saved = new long[poles.size()];
-        Arrays.fill(saved, Integer.MAX_VALUE);
         Collections.sort(poles, new Comparator<Pole>() {
             @Override
             public int compare(Pole o1, Pole o2) {
-                return -Integer.compare(o1.alt, o2.alt);
+                return Integer.compare(o1.alt, o2.alt);
             }
         });
-        return _cost(k, poles, 0);
+        return _cost(k, poles, -1);
     }
 
-    private long _cost(int k, List<Pole> poles, int sI) {
-        if (k > poles.size() || (k == 0 && poles.size() > 0)) {
-            return Integer.MAX_VALUE;
-        } else if (k == 0 && poles.size() == 0) {
-            return 0;
+    private long _cost(int k, List<Pole> poles, int prevAlt) {
+        if (poles.size() == 0) return 0;
+        Pole pole = poles.get(0);
+        if (prevAlt >= 0) {
+            long sumA = (pole.alt - prevAlt) * pole.weight + _cost(k, poles.subList(1, poles.size()), prevAlt);
+            if (k > 0) {
+                long sumB = _cost(k - 1, poles.subList(1, poles.size()), pole.alt);
+                return Math.min(sumA, sumB);
+            } else {
+                return sumA;
+            }
+        } else {
+            return _cost(k - 1, poles.subList(1, poles.size()), pole.alt);
         }
-
-        for (int i = 0; i < poles.size(); i++) {
-            long moveSum = calcMoveSum(poles, i);
-            long subSum = _cost(k - 1, poles.subList(i + 1, poles.size()), sI + 1);
-            saved[sI] = Math.min(saved[sI], moveSum + subSum);
-        }
-        return saved[sI];
     }
-
-    private long calcMoveSum(List<Pole> poles, int end) {
-        long s = 0;
-        int alt = poles.get(end).alt;
-        for (int i = 0; i < end; i++) {
-            Pole pole = poles.get(i);
-            s += (pole.alt - alt) * pole.weight;
-        }
-        return s;
-    }
-
 
     static class Pole {
         final int alt;
